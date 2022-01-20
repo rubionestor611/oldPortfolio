3e88e1e24cfde31a7f572b4665220c2d7eb6af68
Nestor Rubio <nestordrubio9@gmail.com>
2022-01-20 12:44:14 -0500

    effects on work exp and started stock researcher but no server -> no api call

--- JS/workexp.js
+++ JS/workexp.js
@@ -1,4 +1,24 @@
+function init() {
+    let floating = document.getElementsByClassName('float');
+    for (let index = 0; index < floating.length; index++) {
+        floating[index].addEventListener('mouseover', function(event){
+            event.target.classList.remove("float");
+        });
 
-function init(){
-    
+        floating[index].addEventListener('mouseout', function(event){
+            event.target.classList.add("float");
+        });
+
+        let children = floating[index].childNodes;
+        for(let i = 0; i < children.length; i++) {
+            children[i].addEventListener('mouseover', function(event){
+                floating[index].classList.remove("float");
+            });
+            children[i].addEventListener('mouseout', function(event){
+                floating[index].classList.add("float");
+            });
+        }
+    }
 }
+
+init();
